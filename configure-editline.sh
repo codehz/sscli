ef79d2480aacd1bd3d26251e2306108f41bc6302
Code Hz <CodeHz@outlook.com>
2019-04-08 17:47:01 +0800

    fix static link

--- configure-editline.sh
+++ configure-editline.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -ex
-for i in $*; do
-  echo $i
-  eval "export $i"
+#!/bin/bash
+for i in "${@}"; do
+  eval export ${i%=*}=\"${i#*=}\"
 done
+set -ex
 rsync $SRC/editline/ . --delete -r
 ./autogen.sh
 ./configure --prefix=$DST/editline
